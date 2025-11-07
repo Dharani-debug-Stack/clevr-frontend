8c3fd06242ff6dbe2a99cf85758a43f23c0d8160
Dharani Arungulavan <144215712+Dharani-debug-Stack@users.noreply.github.com>
2025-11-07 13:54:24 +0530

    chatbot update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import BestSeller from "../components/home/bestSeller/BestSeller"
 import Collections from "../components/home/collections/Collections"
 import Testtinomials from "../components/home/testinomials/Testimonials"
 import Footer from "../components/home/footer/Footer"
+import Chatbot from "../components/Chatbot"
 
 
 
@@ -14,20 +15,22 @@ const Home = () => {
   return (
     <div className="overflow-x-hidden">
       <Navbar />
-      <HeroSection/>
+      <HeroSection />
       <div id="book">
-        <FeatureBook/>
-        </div>
-        <div id="about"> <Services/></div>
-
+        <FeatureBook />
+      </div>
+      <div id="about">
+        <Services />
+      </div>
       <div id="seller">
-     <BestSeller/>
-     </div>
-     
-     <Collections/>
-    
-     <Testtinomials/>
-     <Footer/>
+        <BestSeller />
+      </div>
+      <Collections />
+      <Testtinomials />
+      <Footer />
+
+      {/* 🧠 Clevr Chatbot */}
+      <Chatbot />
     </div>
   )
 }
